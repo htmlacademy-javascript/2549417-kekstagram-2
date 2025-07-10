39f8959b79e0dce3741374d3a086406e0fb4f09c
Keks <keks@htmlacademy.ru>
2025-07-10 05:39:55 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,8 @@
+// import { COUNT_OBJECTS } from './settings.js';
+// import { createPhoto } from './utils.js';
+
+// console.log(Array.from({ length: COUNT_OBJECTS }, createPhoto()));
+
+import { miniaturesView } from './miniatures';
+
+miniaturesView();
